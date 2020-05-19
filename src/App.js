2a0067b837722f9943dcea0bd143146d999f9922
Jose Fernandez <Jose.Z.Fernandez.-ND@disney.com>
2020-05-19 17:46:44 -0500

    Lesson 29 - VS Code

--- src/App.js
+++ src/App.js
@@ -1,35 +1,34 @@
-import React, {Component} from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { CardList } from "./components/card-list/card-list.component.jsx";
+import "./App.css";
 
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            monsters: [],
-            string: 'valsim'
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      string: "valsim",
+    };
+  }
 
-    componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(res => res.json())
-            //.then(lo => console.log(lo));
-            .then(users => this.setState({monsters: users}))
-    }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      //.then(lo => console.log(lo));
+      .then((users) => this.setState({ monsters: users }));
+  }
 
-    render() {
-        return (
-            <div className="App">
-                {
-                    this.state.monsters.map(monster =>
-                        (<h1 key={monster.id}>{monster.name}</h1>))
-                }
-
-            </div>
-        );
-
-    }
-
+  render() {
+    return (
+      <div className="App">
+        <CardList name="Mikes">
+          {this.state.monsters.map((monster) => (
+            <h1 key={monster.id}>{monster.name}</h1>
+          ))}
+        </CardList>
+      </div>
+    );
+  }
 }
 
 export default App;
