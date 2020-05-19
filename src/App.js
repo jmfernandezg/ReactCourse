dfd3a7bb7f5f160161f82c9d28f00ef6ca6c511e
Jose Fernandez <Jose.Z.Fernandez.-ND@disney.com>
2020-05-19 18:57:53 -0500

    Lesson 31 - VS code

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [],
-      string: "valsim",
+      monsters: []
     };
   }
 
@@ -21,10 +20,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CardList name="Mikes">
-          {this.state.monsters.map((monster) => (
-            <h1 key={monster.id}>{monster.name}</h1>
-          ))}
+        <CardList monsters={this.state.monsters}>
         </CardList>
       </div>
     );
---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './card.styles.css'
+
+export const Card = (props) => (
+  <div className='card-container'>
+  <img src={`https://robohash.org/${props.monster.id}?size=180x180&set=4`} />
+  <h2>{props.monster.name}</h2>
+  <p>{props.monster.email}</p>
+  </div>
+)
