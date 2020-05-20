a898c672750a0e2138ecc81b5325ad9e2c4f3f5b
Jose Fernandez <Jose.Z.Fernandez.-ND@disney.com>
2020-05-20 10:36:01 -0500

    Updates

---
+++ precache-manifest.971e93b4bb49646ff62b109573b216b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "900ba7843d25e2440a8dbbe291c476c4",
+    "url": "/ReactCourse/index.html"
+  },
+  {
+    "revision": "6b8f0948281ce4c55f57",
+    "url": "/ReactCourse/static/css/main.da2153e5.chunk.css"
+  },
+  {
+    "revision": "7af98f766ad3362aae75",
+    "url": "/ReactCourse/static/js/2.24b30b52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactCourse/static/js/2.24b30b52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b8f0948281ce4c55f57",
+    "url": "/ReactCourse/static/js/main.25af651d.chunk.js"
+  },
+  {
+    "revision": "b9b3ff1341c4a816b4c7",
+    "url": "/ReactCourse/static/js/runtime-main.decd1b71.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/ReactCourse/precache-manifest.971e93b4bb49646ff62b109573b216b8.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactCourse/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
